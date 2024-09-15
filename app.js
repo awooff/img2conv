4c3935bffbc01784a4659787e299576cfe60ea5f
Kae <5192298-kaelta@users.noreply.gitlab.com>
2024-09-15 06:36:31 +0100

    smol fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const favicon = require('serve-favicon')
 const path = require('path');
+const fs = require('node:fs/promises');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const stylus = require('stylus');
@@ -31,7 +32,7 @@ const storage = multer.diskStorage({
     const ext = require('node:path').extname(file.originalname);
     ext = ext.length > 1 ? ext : '.' + require('mime').extension(file.mimetype);
     require('node:crypto').pseudoRandomBytes(16, (err, raw) => {
-      cb(null, (err ? undefined : raw.toString('hex') )+ ext);
+      cb(null, 'public/uploads'/*(err ? undefined : raw.toString('hex') )+ ext*/);
     });
   }
 });
@@ -41,7 +42,26 @@ const upload = multer({storage});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.post('/processor', upload.array('images'), processorRouter);
+
+function uploadFiles(request, response) {
+  console.log(request.body);
+  console.log(request.files);
+  response.json({message: 'successfully uploaded', status: 200});
+}
+
+async function processFiles() {
+  const folder = path.join(__dirname, folder_name);
+
+  fs.stat(folder, (err, stats) => {
+    if (err)
+      console.error(err);
+  })
+
+  console.log(stats);
+  console.log(`isFile ? ${stats.isFile()}`)
+}
+
+app.post('/processor', upload.array('images'), uploadFiles);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- public/javascripts/capturefiles.js
+++ public/javascripts/capturefiles.js
@@ -3,17 +3,22 @@ form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  const images = document.getElementById('filetype_select');
 
-  uploadFiles();
-}
-
-function uploadFiles() {
-  const url = '/processor';
   const formData = new FormData(form);
+
+  for (let i = 0; i < files.files.length; ++i) {
+    formData.append('files', files.files);
+  }
+
+  const url = '/processor';
   const fetchOptions = {
     method: 'post',
     body: formData
-  }
+  };
 
-  fetch(url, fetchOptions);
+  fetch(url, fetchOptions)
+    .then(response => console.log(response))
+    .catch(err => console.error(err.stack));
 }
+
